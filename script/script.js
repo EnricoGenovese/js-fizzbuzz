7c315b84c85e3a6cb4d71b1e905f0c3302e906bf
EnricoGenovese <enricogeno@libero.it>
2024-10-29 13:41:10 +0100

    init

---
+++ script/script.js
@@ -0,0 +1,17 @@
+"use strict"
+console.clear;
+
+/* Consegna:
+Scrivi un programma che stampi i numeri da 1 a 100,
+ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.                       */
+
+// L'esercizio necessita di un ciclo 'for', con innestato all'interno un ciclo if per la condizione
+// più stringente (multiplo di due interi); le altre due condizioni vanno analizzate separatamente.
+// Il restante degli interi va stampato come tale in console.
+
+// 1- Known variables
+
+// L'unica variabile (o meglio costante) necessaria è il numero più alto da restituire a terminale;
+// questo fungerà da valore per il quale il ciclo 'for' interromperà l'iterazione.
+
